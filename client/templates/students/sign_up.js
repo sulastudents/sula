5d47efc7a329190e9ddfeb3d54fd5b1970e78948
3210jr <3210jr@gmail.com>
2015-11-15 19:52:24 +0300

    form validations, form bug fixes

--- client/templates/students/sign_up.js
+++ client/templates/students/sign_up.js
@@ -1,11 +1,11 @@
 // Template.signUp.created = () => {
-    // let instance = this;
-    //
-    // let subscription = instance.subscribe('profile', userId);
-    //
-    // instance.profile = () => {
-    //     return Profiles.findOne({});
-    // };
+// let instance = this;
+//
+// let subscription = instance.subscribe('profile', userId);
+//
+// instance.profile = () => {
+//     return Profiles.findOne({});
+// };
 // };
 
 Template.signUp.rendered = function() {
@@ -13,17 +13,32 @@ Template.signUp.rendered = function() {
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 80 // Creates a dropdown of 15 years to control year
     });
+
+    $(document).ready(function() {
+        $('select').material_select();
+    });
+
 };
 
 Template.signUp.events({
     "change #studentsAcceptTerms": function (event) {
         var checkbox = document.getElementById('studentsAcceptTerms');
         var registerBtn = document.getElementById('studentSignUpBtn');
-            if(checkbox.checked) {
-                $('#studentSignUpBtn').removeClass('disabled');
-            } else {
-                registerBtn.classList ? registerBtn.classList.add('disabled') : registerBtn.className += ' disabled';
-            }
+        // var verifyInfo = document.getElementById('verifyInfo');
+        if(checkbox.checked && $('#verifyInfo').prop('checked')) {
+            $('#studentSignUpBtn').removeClass('disabled');
+        } else {
+            registerBtn.classList ? registerBtn.classList.add('disabled') : registerBtn.className += ' disabled';
+        }
+    },
+    "change #verifyInfo": function (event) {
+        var checkbox = document.getElementById('verifyInfo');
+        var registerBtn = document.getElementById('studentSignUpBtn');
+        if(checkbox.checked && $('#studentsAcceptTerms').prop('checked')) {
+            $('#studentSignUpBtn').removeClass('disabled');
+        } else {
+            registerBtn.classList ? registerBtn.classList.add('disabled') : registerBtn.className += ' disabled';
+        }
     },
     'submit form': function(event, template) {
         event.preventDefault();
@@ -41,13 +56,23 @@ Template.signUp.events({
             phnNumber: $('#phnNumber').val(),
             school: $('#school').val(),
             studentNumber: $('#studentNumber').val(),
-            gradDate: $('#gradDate').val()
+            gradDate: $('#gradDate').val(),
+            gender: $('#gender').val(),
+            delivery: $('#delivery').val(),
+            payment: $('#payment').val()
 
             // photo removed because cards are no longer printed with photoes
             // photo: $('#preview')[0].src
         };
 
+        if (!SulaUtils.validateStudentSignupForm(form)) {
+            toastr.error("Please complete the form");
+            return;
+        }
+
         // console.log(form);
+        // console.log(SulaUtils.validateStudentSignupForm(form));
+        // return;
 
         Meteor.call('insertStudent', form, function(error, result) {
             if (error) {
@@ -63,30 +88,30 @@ Template.signUp.events({
     'change #photo': function(event, template) {
         encodeImageFileAsURL = function (filesSelected){
 
-			    // var filesSelected = document.getElementById('newImg').files;
-			    if (filesSelected.length > 0)
-			    {
-			        var fileToLoad = filesSelected[0];
+            // var filesSelected = document.getElementById('newImg').files;
+            if (filesSelected.length > 0)
+            {
+                var fileToLoad = filesSelected[0];
 
-			        var fileReader = new FileReader();
+                var fileReader = new FileReader();
 
-			        fileReader.onload = function(fileLoadedEvent) {
-			            var srcData = fileLoadedEvent.target.result; // <--- data: base64
+                fileReader.onload = function(fileLoadedEvent) {
+                    var srcData = fileLoadedEvent.target.result; // <--- data: base64
 
-			                    // var newImage = document.createElement('img');
-			                    // newImage.src = srcData;
+                    // var newImage = document.createElement('img');
+                    // newImage.src = srcData;
 
-			                    // document.getElementById("imgTest").innerHTML = newImage.outerHTML;
-			                    // alert("Converted Base64 version is "+document.getElementById("imgTest").innerHTML);
-			                    console.log(srcData);
-			            $('#preview').attr('src', srcData);
-			            return srcData;
-			        };
-			        fileReader.readAsDataURL(fileToLoad);
+                    // document.getElementById("imgTest").innerHTML = newImage.outerHTML;
+                    // alert("Converted Base64 version is "+document.getElementById("imgTest").innerHTML);
+                    console.log(srcData);
+                    $('#preview').attr('src', srcData);
+                    return srcData;
+                };
+                fileReader.readAsDataURL(fileToLoad);
 
-			    }
-			};
+            }
+        };
 
-		encodeImageFileAsURL(document.getElementById('photo').files);
+        encodeImageFileAsURL(document.getElementById('photo').files);
     }
 });
--- client/templates/students/thumb.js
+++ client/templates/students/thumb.js
@@ -6,3 +6,21 @@ Template.thumb.events({
 		$('#myModal').modal('show');
 	}
 });
+
+Template.thumb.helpers({
+	'offerImageAlt': function () {
+		console.log(generateSEOKeyword());
+		return generateSEOKeyword();
+	}
+});
+
+function generateSEOKeyword () {
+	var keywords = [
+		"University Students", "Student Deals", "Student Offers", "Student Discounts",
+		"Sula Students", "Sula Card", "Do More", "Tanzania", "Entertainment", "offers",
+		"discounts", "Tanzanian Students", "Students"];
+
+	var index = Math.floor(Math.random() * 12);
+
+	return keywords[index];
+}
